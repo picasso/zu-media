749c716e54156b27889f91e6dae8d1294375f116
picasso <dmitry.rudakov@icloud.com>
2021-04-13 18:58:00 +0200

    fixed bug with reloading 'Sizes' table

--- scripts/settings/sizes.js
+++ scripts/settings/sizes.js
@@ -13,10 +13,20 @@ const ZumediaSizes = ({
 		setUpdateHook,
 }) => {
 
+	const [isOpen, setIsOpen] = useState(false);
 	const [sizes, setSizes] = useState(null);
 
 	const onToggle = useCallback(() => {
-		if(sizes === null) {
+		setIsOpen(prev => !prev);
+	}, []);
+
+	// reset sizes table when 'responsive' or 'full_hd' option is updated
+	useEffect(() => {
+		setUpdateHook(['responsive', 'full_hd'], () => {
+			setSizes(null);
+		});
+		if(isOpen && sizes === null) {
+			// request data for table of sizes
 			ajaxAction('zumedia_all_sizes', sizesData => {
 				const headers = get(sizesData, 'headers', []);
 				const rows = get(sizesData, 'rows', []);
@@ -24,26 +34,18 @@ const ZumediaSizes = ({
 				if(rows.length) setSizes({ config, headers, rows });
 			});
 		}
-
-	}, [ajaxAction, sizes]);
-
-	// reset sizes table when 'responsive' or 'full_hd' option is updated
-	useEffect(() => {
-		setUpdateHook(['responsive', 'full_hd'], () => {
-			setSizes(null);
-		});
-	}, [setSizes, setUpdateHook]);
+	}, [isOpen, sizes, setUpdateHook, ajaxAction]);
 
 	return (
-			<ZukitPanel id="sizes" initialOpen={ false } onToggle={ onToggle }>
-				<ZukitTable
-					fixed={ true }
-					config={ sizes && sizes.config }
-					head={ sizes && sizes.headers }
-					body={ sizes && sizes.rows }
-					loading={ sizes === null }
-				/>
-			</ZukitPanel>
+		<ZukitPanel id="sizes" initialOpen={ isOpen } onToggle={ onToggle }>
+			<ZukitTable
+				fixed={ true }
+				config={ sizes && sizes.config }
+				head={ sizes && sizes.headers }
+				body={ sizes && sizes.rows }
+				loading={ sizes === null }
+			/>
+		</ZukitPanel>
 	);
 };
 
